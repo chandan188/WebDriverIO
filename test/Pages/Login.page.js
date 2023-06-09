33a73edc6e8ec9df01c022c7cc4d04d0c1b80dd1
cd21992 <cd21992@essex.ac.uk>
2023-06-09 01:45:34 +0100

    POM approach LoginPage

--- test/Pages/Login.page.js
+++ test/Pages/Login.page.js
@@ -1,3 +1,26 @@
 class LoginPage{
-    
+
+    get usernameField(){
+
+        return $("[name='username']")
+
+    }
+
+    get passwordField(){
+
+        return $("[name='password']")
+    }
+
+    get submitBtn(){
+        return $("button[type='submit']")
+    }
+
+   async login(userName, password){
+        await this.usernameField().setValue(userName)
+        await this.passwordField().setValue(password)
+        await this.submitBtn().click()
+    }
+
 }
+ 
+module.exports =  new LoginPage()
--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -1,11 +1,19 @@
+const loginPage = require('../Pages/Login.page')
+
 describe("First test suite", () => {
     it("Launch Browser", async () => {
 
-        browser.url("https://www.google.com")
-        await $("button[id='L2AGLb']").click()
-        await $("[name='q']").setValue("webdriver io")
-        browser.pause(5000)
-        browser.keys("Enter")
-        browser.pause(15000)
+        // browser.url("https://www.google.com")
+        // await $("button[id='L2AGLb']").click()
+        // await $("[name='q']").setValue("webdriver io")
+        // browser.pause(5000)
+        // browser.keys("Enter")
+        // browser.pause(15000)
+
+        browser.url("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        loginPage.login("Admin", "admin123")
+
+        
+
     })
 })
